9d74b16a9481882006c65b9bdac12c5b1fee9798
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-11 17:57:04 -0300

    Manejo de rutas con ruteador agregado (se puede mejorar? seguramente)

--- public/index.php
+++ public/index.php
@@ -2,18 +2,23 @@
 
 require __DIR__ . '/../src/bootstrap.php';
 
-#use Paw\Core\Router; 
-use Paw\App\Controllers\PageController;
-
+use Paw\Core\Exceptions\RouteNotFoundException;
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $log->info("Peticion a: {$path}");
 
-#$router = new Router;
-#$router->loadRoutes();
-#$router->direct($path);
+try{
+    $router->direct($path);
+    $log->info("Respuesta exitosa: 200 - {$path}");
+} catch (RouteNotFoundException $e){
+    $router->direct('not_found');
+    $log->info("Path not found: 404 - Route not found", ["Path" => $path]);
+} catch (Exception $e){
+    $router->direct("internal_error");
+    $log->error("Status code:500 - Internal server error", ["Error" => $e]);
+}
 
-$controller = new PageController;
+/*$controller = new PageController;
 
 if ($path == '/'){
     $controller->index();
@@ -21,5 +26,5 @@ if ($path == '/'){
 } else {
     $controller->rutas($path);
     $log->info("Respuesta exitosa: 200");
-}
+}*/
 
--- src/App/Controllers/PageController.php
+++ src/App/Controllers/PageController.php
@@ -60,16 +60,96 @@ class PageController
 
     public function index()
     {
-
         $titulo = htmlspecialchars($_GET['nombre'] ?? "PAW");
         require $this->viewsDir . 'index.view.php';
     }
 
-    public function rutas($path)
+    /*--------------COMPRA------------------*/
+
+    public function menu()
+    {
+        require $this->viewsDir . 'compra/menu.view.php';
+    }
+
+    public function carrito()
+    {
+        require $this->viewsDir . 'compra/carrito.view.php';
+    }
+
+    public function confirmarCompra()
+    {
+        require $this->viewsDir . 'compra/confirmarCompra.view.php';
+    }
+
+    public function pedirComida()
+    {
+        require $this->viewsDir . 'compra/pedirComida.view.php';
+    }
+
+    public function reserva()
+    {
+        require $this->viewsDir . 'compra/reserva.view.php';
+    }
+
+    public function selecDirec()
+    {
+        require $this->viewsDir . 'compra/selecDirec.view.php';
+    }
+
+    public function selecLoc()
+    {
+        require $this->viewsDir . 'compra/selecLoc.view.php';
+    }
+
+    /*--------------CUENTA------------------*/
+
+    public function agregarDireccion()
+    {
+        require $this->viewsDir . 'cuenta/agregarDireccion.view.php';
+    }
+
+    public function consumos()
+    {
+        require $this->viewsDir . 'cuenta/consumos.view.php';
+    }
+
+    public function login()
+    {
+        require $this->viewsDir . 'cuenta/login.view.php';
+    }
+
+    public function perfil()
+    {
+        require $this->viewsDir . 'cuenta/perfil.view.php';
+    }
+
+    public function registrarse()
+    {
+        require $this->viewsDir . 'cuenta/registrarse.view.php';
+    }
+
+    /*--------------INSTITUCIONAL------------------*/
+
+    public function locales()
+    {
+        require $this->viewsDir . 'institucional/locales.view.php';
+    }
+
+    public function nosotros()
+    {
+        require $this->viewsDir . 'institucional/nosotros.view.php';
+    }
+
+    public function servCliente()
+    {
+        require $this->viewsDir . 'institucional/servCliente.view.php';
+    }
+
+    /*public function rutas($path)
     {
         $ruta_sin_barra = ltrim($path, '/');
 
         $titulo = htmlspecialchars($_GET['nombre'] ?? "PAW");
         require $this->viewsDir . $ruta_sin_barra . '.view.php';
-    }
+    }*/
 }
