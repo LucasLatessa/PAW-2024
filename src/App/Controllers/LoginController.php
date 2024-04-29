f7eb77a056d1230ee898cfb07c05c8003c603c6c
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-29 20:27:52 -0300

    Manejo de rutas mejorado (falta terminar)

--- src/App/Controllers/LoginController.php
+++ src/App/Controllers/LoginController.php
@@ -2,68 +2,13 @@
 
 namespace Paw\App\Controllers;
 use Paw\App\Models\Usuario;
+use Paw\Core\Controlador;
 
-class LoginController{
-
-    public string $viewsDir; #Direccion a la vista indicada
-
+class LoginController extends Controlador{ 
     private $usuariosJSON = __DIR__ . "/../data/usuarios.json";
+    public string $viewsDir; #Direccion a la vista indicada
     public function __construct(){
-
-        
-        $this->viewsDir = __DIR__ . "/../views/";
-
-        $this->rutasMenuBurger = [
-            /* Menu hamburguesa 0-2*/ 
-            [
-                "href" => '../compra/menu',
-                "name" => "Menu",
-            ],
-            [
-                "href" => '../compra/reserva',
-                "name" => "Reserva mesa",
-            ],
-            [
-                "href" => '../cuenta/perfil',
-                "name" => "Perfil"
-            ]
-            ];
-        $this->rutasLogoHeader =
-            /*Logo header 3*/
-            [
-                "href" => '../',
-                "name" => "Home",
-            ];
-        $this->rutasHeaderDer = [
-            /*Header parte derecha 4-5*/
-            [
-                "href" => '../compra/carrito',
-                "name" => "carrito",
-            ],
-            [
-                "href" => '../cuenta/login',
-                "name" => "usuario"
-            ],
-        ];
-        $this->rutasFooter = [
-            /*Footer 6-9*/
-            [
-                "href" => '../institucional/locales',
-                "name" => "Locales"
-            ],
-            [
-                "href" => '../institucional/servCliente',
-                "name" => "Servicio al Cliente"
-            ],
-            [
-                "href" => '../institucional/nosotros',
-                "name" => "Sobre nosotros"
-            ],
-            [
-                "href" => '../cuenta/consumos',
-                "name" => "Consumos"
-            ]
-            ];
+        parent::__construct();
     }
 
     public function registrarse(){
--- src/App/Controllers/MenuController.php
+++ src/App/Controllers/MenuController.php
@@ -2,66 +2,13 @@
 
 namespace Paw\App\Controllers;
 use Paw\App\Models\Plato;
+use Paw\Core\Controlador;
 
-class MenuController
+class MenuController extends Controlador
 {
     public string $viewsDir; #Direccion a la vista indicada
     public function __construct(){
-
-        
-        $this->viewsDir = __DIR__ . "/../views/";
-
-        $this->rutasMenuBurger = [
-            /* Menu hamburguesa 0-2*/ 
-            [
-                "href" => '../compra/menu',
-                "name" => "Menu",
-            ],
-            [
-                "href" => '../compra/reserva',
-                "name" => "Reserva mesa",
-            ],
-            [
-                "href" => '../cuenta/perfil',
-                "name" => "Perfil"
-            ]
-            ];
-        $this->rutasLogoHeader =
-            /*Logo header 3*/
-            [
-                "href" => '../',
-                "name" => "Home",
-            ];
-        $this->rutasHeaderDer = [
-            /*Header parte derecha 4-5*/
-            [
-                "href" => '../compra/carrito',
-                "name" => "carrito",
-            ],
-            [
-                "href" => '../cuenta/login',
-                "name" => "usuario"
-            ],
-        ];
-        $this->rutasFooter = [
-            /*Footer 6-9*/
-            [
-                "href" => '../institucional/locales',
-                "name" => "Locales"
-            ],
-            [
-                "href" => '../institucional/servCliente',
-                "name" => "Servicio al Cliente"
-            ],
-            [
-                "href" => '../institucional/nosotros',
-                "name" => "Sobre nosotros"
-            ],
-            [
-                "href" => '../cuenta/consumos',
-                "name" => "Consumos"
-            ]
-            ];
+        parent::__construct();
     }
     private function validarImage($tamaño_archivo,$nombreArchivo,$archivoTemporal){
             $limite_tamaño = 1048576; // 1MB en bytes
