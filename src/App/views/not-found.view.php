70ab370176460ada6bec9767d6f1b89cf26ea7ff
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-29 19:32:02 -0300

    Agregadas vista internal error y not found

--- src/App/views/not-found.view.php
+++ src/App/views/not-found.view.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php require __DIR__ . '/parts/head.view.php'; ?>
     <link rel="stylesheet" href="/assets/styles/global.css" />
 </head>
+
 <body>
     <!--HEADER-->
     <?php require __DIR__ . '/parts/header.view.php' ?>
-    <h1>Pagina no encontrada</h1>   
+    <h1 class="titulo-promociones">Pagina no encontrada</h1>
     <!--FOOTER-->
     <?php require __DIR__ . '/parts/footer.view.php' ?>
 </body>
+
 </html>
