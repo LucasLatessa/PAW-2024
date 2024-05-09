534194fba4d690dd754fa1f092583fcbc01f201f
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-08 23:48:56 -0300

    Pintar mesas

--- public/assets/js/appReservas.js
+++ public/assets/js/appReservas.js
@@ -2,7 +2,7 @@ class appReservas{
     constructor(){
         document.addEventListener("DOMContentLoaded", () => {
             PAW.cargarScript("PAWReservas", "/assets/js/components/paw-reservas.js",() => {
-                let reservas = new PAWReservas(".planoSucursal"); //Le paso el SVG
+                let reservas = new PAWReservas(".planoSucursal", "/assets/js/components/reservas.json"); //Le paso el SVG y la lista de reservas
             });
         });
     }
--- public/assets/js/components/paw-reservas.js
+++ public/assets/js/components/paw-reservas.js
@@ -1,5 +1,5 @@
 class PAWReservas {
-  constructor(svg) {
+  constructor(svg, listaReservas) {
     //Obtengo el svg (que esta como imagen)
     let contenedor = svg.tagName ? pContenedor : document.querySelector(svg);
 
@@ -40,7 +40,74 @@ class PAWReservas {
         //Cambio la mesa que selecciono
         nombreMesa.value = code;
       };
-    } else { //En el caso de que no exista el SVG
+
+      /*OBTENGO TODAS LAS MESAS OCUPADAS*/
+      const reservas = [];
+
+      //Cargo la lista de reservas
+      const getReservas = () => {
+        fetch(listaReservas)
+          .then((response) => response.json())
+          .then((data) => {
+            reservas.push(...data);
+            mesasOcupadas(reservas); /*SOLUCIONAR: Problema con mesas ocupadas*/
+          });
+      };
+
+      getReservas();
+      //console.log(reservas);
+
+      /*PINTAR MESAS OCUPADAS*/
+      const mesasOcupadas = (misReservas) => {
+        const diaReserva = document.querySelector("input[name='dia']");
+        //const diaReserva = "20/05/2024";
+        const horarioReserva = document.querySelector("input[name='horario']");
+
+        //Pinta las mesas ocupadas
+        for (let i = 0; i < reservas.length; i++) {
+          const mesa = document.getElementById(reservas[i].mesa);
+          const elementosMesa = mesa.querySelectorAll("*");
+
+          if (reservas[i].dia == diaReserva.value) {
+            // Obtener todos los elementos dentro del grupo SVG
+            // Iterar sobre cada elemento y piso los estilos que tiene
+            elementosMesa.forEach((elemento) => {
+              elemento.classList.add("ocupado");
+              elemento.classList.remove("mesa");
+            });
+          } else {
+            elementosMesa.forEach((elemento) => {
+              elemento.classList.remove("ocupado");
+              elemento.classList.add("mesa");
+            });
+          }
+        }
+      };
+
+      //mesasOcupadas();
+      // Agregar un evento 'change' al elemento diaReserva
+      // diaReserva.addEventListener("change", function (event) {
+      //   // La función se ejecutará cuando el valor del input cambie
+      //   console.log("El valor del input ha cambiado a:", event.target.value);
+      //   // Aquí puedes agregar más acciones que desees realizar cuando cambie el valor del input
+      // });
+      //console.log(diaReserva);
+
+      const diaReserva = document.querySelector("input[name='dia']");
+      //console.log(diaReserva)
+      diaReserva.addEventListener("change", function (event) {
+        console.log("El valor del input ha cambiado a:", event.target.value);
+        getReservas();
+      });
+
+      /*
+    g[id^="mesa-"]:hover .mesa {
+      fill: #ffaf10;
+      cursor: pointer;
+    } 
+    */
+    } else {
+      //En el caso de que no exista el SVG
       console.log("No existe");
     }
   }
