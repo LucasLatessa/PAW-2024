7f5d3782f1c49644aa8e9fc86b1987177349d8c7
lucaslatessa <lucaslatessa@gmail.com>
2024-05-09 19:55:11 -0300

    Implementado Json en turnera, ya funciona

--- public/assets/js/components/paw-turnera.js
+++ public/assets/js/components/paw-turnera.js
@@ -1,21 +1,23 @@
 class PAWTurnera{
-    constructor(){
+    constructor(pedidos){
         console.log("turnera");
-        this.generarTurnos();
-        this.actualizarTablaPedido();
+        this.generarTurnos(pedidos);
+        this.actualizarTablaPedido(pedidos);
     }
 
     //FUNCIONES 
-    generarTurnos(){
-        const pedidos = [
+    async generarTurnos(pedidos){
+        /* const pedidos = [
             { numero: "0001A"},
             { numero: "00004A"},
             { numero: "0008B"}
-        ];
-        
+        ]; */
+        const response = await fetch(pedidos);
+        const data = await response.json();
+
         let retirosSection = document.querySelector(".section-retiros");
         
-        pedidos.forEach(pedido => {
+        data.forEach(pedido => {
             let nuevoArticulo = document.createElement("article");
             nuevoArticulo.textContent = pedido.numero;
             retirosSection.appendChild(nuevoArticulo);
@@ -23,16 +25,19 @@ class PAWTurnera{
         
     }
 
-    actualizarTablaPedido(){
-        const pedidoEstado = [
+    async actualizarTablaPedido(pedidos){
+       /* const pedidoEstado = [
             { numero: "0001A", estado:"Listo para retirar"},
             { numero: "0002B", estado:"En preparación"},
             { numero: "0003A", estado:"En preparación"}
-        ];
+        ]; */
+        const response = await fetch(pedidos);
+        const data = await response.json();
+
 
         let pedidosSection = document.querySelector(".pedido-estado");
         let tabla = pedidosSection.querySelector("table");
-        pedidoEstado.forEach(pedido => {
+        data.forEach(pedido => {
             let nuevoPedido = document.createElement("tr");
             let numeroPedido = document.createElement("td");
             let estadoPedido = document.createElement("td");
