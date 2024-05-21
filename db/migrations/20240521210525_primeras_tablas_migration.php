769304b821b641751e95314d68aac1738c132ad7
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-21 18:09:30 -0300

    Comienzo BD

---
+++ db/migrations/20240521210525_primeras_tablas_migration.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class PrimerasTablasMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $plato = $this->table('plato');
+        $plato->addColumn('nombre','string', ['limit' => 60])
+        ->addColumn('descripcion','string', ['limit' => 60])
+        ->addColumn('precio','integer', ['limit' => 60])
+        ->create();
+    }
+}
